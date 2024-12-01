785ad98ef6e0a02aa147ed84b15d30c4551dfba1
RogerAAG <rogeraag.dev@gmail.com>
2024-11-30 23:42:50 -0500

    etiquetas

--- FormTransferencias.Designer.cs
+++ FormTransferencias.Designer.cs
@@ -31,6 +31,7 @@
             this.lblLlamada = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panelGrafo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,10 +58,21 @@
             // 
             // panelGrafo
             // 
-            this.panelGrafo.Location = new System.Drawing.Point(448, 82);
+            this.panelGrafo.Location = new System.Drawing.Point(467, 139);
             this.panelGrafo.Name = "panelGrafo";
             this.panelGrafo.Size = new System.Drawing.Size(431, 416);
             this.panelGrafo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 150);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "1. Atención al Cliente. (AC)\r\n2. Facturación. (F)\r\n3. Soporte Tecnico. (ST)\r\n4. R" +
+    "ecursos Humanos. (R.R.H.H)\r\n5. TI Interno. (TI)\r\n6. Administración. (A)";
             // 
             // FormTransferencias
             // 
@@ -68,6 +80,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(201)))), ((int)(((byte)(217)))));
             this.ClientSize = new System.Drawing.Size(950, 635);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panelGrafo);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -76,6 +89,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -84,5 +98,6 @@
         private System.Windows.Forms.Label lblLlamada;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panelGrafo;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FormTransferencias.cs
+++ FormTransferencias.cs
@@ -9,7 +9,7 @@ namespace GRUPO01_EF_ESTDAT_UPN
     {
         private GrafoTransferencias grafo;
 
-        public FormTransferencias()
+        public FormTransferencias()//Constructor
         {
             InitializeComponent();
             InicializarGrafo();
@@ -18,18 +18,18 @@ namespace GRUPO01_EF_ESTDAT_UPN
             panelGrafo.Paint += new PaintEventHandler(PanelGrafo_Paint);
         }
 
-        private void InicializarGrafo()
+        private void InicializarGrafo()//Inicializa el grafo
         {
-            grafo = new GrafoTransferencias();
+            grafo = new GrafoTransferencias();//Instancia de la clase GrafoTransferencias
 
-            var ac = grafo.AgregarNodo("Atención al Cliente");
+            var ac = grafo.AgregarNodo("Atención al Cliente");//Agrega un nodo
             var f = grafo.AgregarNodo("Facturación");
             var st = grafo.AgregarNodo("Soporte Técnico");
             var rh = grafo.AgregarNodo("Recursos Humanos");
             var ti = grafo.AgregarNodo("TI Interno");
             var a = grafo.AgregarNodo("Administración");
 
-            grafo.AgregarArista(ac, f, 3);
+            grafo.AgregarArista(ac, f, 3);//Agrega una arista
             grafo.AgregarArista(ac, st, 4);
             grafo.AgregarArista(f, rh, 4);
             grafo.AgregarArista(st, ti, 3);
@@ -37,20 +37,20 @@ namespace GRUPO01_EF_ESTDAT_UPN
             grafo.AgregarArista(ti, a, 5);
         }
 
-        private void PanelGrafo_Paint(object sender, PaintEventArgs e)
+        private void PanelGrafo_Paint(object sender, PaintEventArgs e)//Dibuja el grafo
         {
-            Graphics g = e.Graphics;
-            Pen pen = new Pen(Color.Blue, 2);
-            Font font = new Font("Century Gothic", 11);
-            Brush brush = Brushes.Black;
+            Graphics g = e.Graphics;//Instancia de la clase Graphics
+            Pen pen = new Pen(Color.Blue, 2);//Instancia de la clase Pen
+            Font font = new Font("Century Gothic", 11);//Instancia de la clase Font
+            Brush brush = Brushes.Black;//Instancia de la clase Brush
 
             // Tamaño y posición ajustados al panel
             var ancho = panelGrafo.Width;
             var alto = panelGrafo.Height;
 
-            var posiciones = new Dictionary<string, Point>
+            var posiciones = new Dictionary<string, Point>//Instancia de la clase Dictionary
             {
-                { "Atención al Cliente", new Point(ancho / 2, alto / 8) },
+                { "Atención al Cliente", new Point(ancho / 2, alto / 8) },//Agrega un nodo
                 { "Facturación", new Point(ancho / 4, alto / 3) },
                 { "Soporte Técnico", new Point(3 * ancho / 4, alto / 3) },
                 { "Recursos Humanos", new Point(ancho / 4, 2 * alto / 3) },
@@ -59,24 +59,24 @@ namespace GRUPO01_EF_ESTDAT_UPN
             };
 
             // Dibujar conexiones
-            foreach (var nodo in grafo.Nodos)
+            foreach (var nodo in grafo.Nodos)//Recorre los nodos
             {
-                foreach (var conexion in nodo.Conexiones)
+                foreach (var conexion in nodo.Conexiones)//Recorre las conexiones
                 {
-                    Point origen = posiciones[nodo.Departamento];
-                    Point destino = posiciones[conexion.Key.Departamento];
-                    g.DrawLine(pen, origen, destino);
-                    g.DrawString($"{conexion.Value} min", font, brush,
-                                 (origen.X + destino.X) / 2,
-                                 (origen.Y + destino.Y) / 2);
+                    Point origen = posiciones[nodo.Departamento];//Obtiene la posición del nodo
+                    Point destino = posiciones[conexion.Key.Departamento];//Obtiene la posición de la conexión
+                    g.DrawLine(pen, origen, destino);//Dibuja la línea
+                    g.DrawString($"{conexion.Value} min", font, brush,//Dibuja el peso
+                                 (origen.X + destino.X) / 2,//Posición X
+                                 (origen.Y + destino.Y) / 2);//Posición Y
                 }
             }
 
             // Dibujar nodos
-            foreach (var pos in posiciones)
+            foreach (var pos in posiciones)//Recorre las posiciones
             {
-                g.FillEllipse(Brushes.Blue, pos.Value.X - 15, pos.Value.Y - 15, 30, 30);
-                g.DrawString(pos.Key, font, Brushes.White, pos.Value.X - 40, pos.Value.Y - 25);
+                g.FillEllipse(Brushes.Blue, pos.Value.X - 15, pos.Value.Y - 15, 30, 30);//Dibuja el nodo
+                g.DrawString(pos.Key, font, Brushes.White, pos.Value.X - 40, pos.Value.Y - 25);//Dibuja el nombre del nodo
             }
         }
     }
